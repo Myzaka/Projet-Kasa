c1b3f65dd66c7c3a0db080edd9f8f1f3ec14d0d5
Jerome <jerome.semik@bbox.fr>
2023-06-25 21:12:08 +0200

    Rating etoiles et couleurs

--- kasa/src/components/ficheLogement/ficheLogement__host.jsx
+++ kasa/src/components/ficheLogement/ficheLogement__host.jsx
@@ -11,7 +11,12 @@ export default function Host({index}) {
             <img className="host__description--picture" src={biensImmobiliers[index].host.picture} alt={biensImmobiliers[index].host.name} />
         </div>
         <p className='host__rating'>
-            {biensImmobiliers[index].rating}
+            {Array.from({ length: 5 }).map((_, i) => (
+                <i
+                    key={i}
+                    className={`fa-solid fa-star ${i < biensImmobiliers[index].rating ? 'active' : ''}`}
+                ></i>
+                ))}
         </p>
     </div>)
 }
