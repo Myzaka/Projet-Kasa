47c024a924ef6c569a7b50af2384b7014289a71b
Jerome <jerome.semik@bbox.fr>
2023-06-28 08:30:01 +0200

    Header centré

--- kasa/src/components/header.jsx
+++ kasa/src/components/header.jsx
@@ -5,18 +5,20 @@ import './../style/header.scss';
 export default function Header() {
     return (
         
-            <header className = 'header'>
-                <h1><LogoKasa /></h1>
-                <nav >
-                    <ul className = 'header__navbar'>
-                        <li>
-                            <Link to={'/'} className = 'header__navbar__link'>Accueil</Link>
-                        </li>
-                        <li>
-                            <Link to={'/Apropos'} className = 'header__navbar__link'>A propos</Link>
-                        </li>
-                    </ul>
-                </nav>
+            <header className="header">
+                <div className="header__conteneur">
+                    <h1><LogoKasa /></h1>
+                    <nav >
+                        <ul className = 'header__conteneur__navbar'>
+                            <li>
+                                <Link to={'/'} className = 'header__conteneur__navbar--link'>Accueil</Link>
+                            </li>
+                            <li>
+                                <Link to={'/Apropos'} className = 'header__conteneur__navbar--link'>A propos</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </header>
         
         );
