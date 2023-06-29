1763e2913faf827e80f39f33effc55ebe29111fb
Jerome <jerome.semik@bbox.fr>
2023-06-29 10:03:47 +0200

    Suppr surlignage liens page accueil

--- kasa/src/components/card.jsx
+++ kasa/src/components/card.jsx
@@ -4,7 +4,7 @@ import ('./../style/card.scss');
 
 export default function Card({photo, titre, identifiant}) {
     return(
-        <Link to={`/ficheLogement/${identifiant}`}>
+        <Link className = "card__lien" to={`/ficheLogement/${identifiant}`}>
             <div className="card">
                 <img className="card__photo" src={photo} alt={titre} />
                 <h2 className="card__titre">{titre}</h2>
