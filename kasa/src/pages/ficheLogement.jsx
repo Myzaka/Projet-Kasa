f062ee8d26604d7793eec338dd8fd11d8734ee54
Jerome <jerome.semik@bbox.fr>
2023-07-02 09:33:37 +0200

    Utilisation useNavigation pour gérer les erreurs

--- kasa/src/pages/ficheLogement.jsx
+++ kasa/src/pages/ficheLogement.jsx
@@ -3,56 +3,59 @@ import Footer2 from "../components/footer2";
 import Carrousel from "../components/ficheLogement/ficheLogement__carrousel";
 import Selection from "../components/ficheLogement/ficheLogement__selection";
 import Charte from "../components/menuDeroulant";
-import { useParams } from "react-router";
+import { useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
 import biensImmobiliers from "../assets/images/biensImmobiliers.json";
 import '../style/ficheLogement/ficheLogement__selection.scss';
 import '../style/ficheLogement/ficheLogement.scss';
 import Host from "../components/ficheLogement/ficheLogement__host";
 
 export default function FicheLogement() {
+  const logementId = useParams().ficheLogementId;
+  const indexRecherche = biensImmobiliers.findIndex(
+    (element) => element.id === logementId
+  );
+  const navigate = useNavigate();
 
-const logementId = useParams().ficheLogementId;
-const indexRecherche = biensImmobiliers.findIndex(element => element.id === logementId);
+  useEffect(() => {
+    if (indexRecherche === -1) {
+      navigate("/pageErreur");
+    }
+  }, [indexRecherche, navigate]);
 
-    return (
-        <>
-            <Header />
-            <Carrousel
-                logementSelectionne = {indexRecherche}
+  let content = null;
+
+  if (indexRecherche !== -1) {
+    content = (
+      <>
+        <Header />
+        <Carrousel logementSelectionne={indexRecherche} />
+        <section className="detailLogement">
+          <Selection index={indexRecherche} />
+          <Host index={indexRecherche} />
+        </section>
+        <section className="menuDeroulantLogement">
+          <div className="menuDeroulantLogement__contenu">
+            <Charte
+              titre="Description"
+              contenu={biensImmobiliers[indexRecherche].description}
             />
-            <section className="detailLogement">
-                <Selection
-                    index = {indexRecherche} //pb de double key à identifier
-                 />
-                <Host
-                    index = {indexRecherche}
-                />
-            </section>
-            <section className="menuDeroulantLogement">
-                <div className="menuDeroulantLogement__contenu">
-                    <Charte
-                        titre = "Description"
-                        contenu = {biensImmobiliers[indexRecherche].description}
-                    />
-                </div>
-                <div className="menuDeroulantLogement__contenu">
-                    <Charte
-                        titre = 'Equipements'
-                        contenu = 
-                        {biensImmobiliers[indexRecherche].equipments.map((biensImmo) => (
-                          <li key={biensImmo}>
-                            {biensImmo}
-                          </li>
-                        ))}
-                      
+          </div>
+          <div className="menuDeroulantLogement__contenu">
+            <Charte
+              titre="Equipements"
+              contenu={biensImmobiliers[indexRecherche].equipments.map(
+                (biensImmo) => (
+                  <li key={biensImmo}>{biensImmo}</li>
+                )
+              )}
+            />
+          </div>
+        </section>
+        <Footer2 />
+      </>
+    );
+  }
 
-                    />    
-                </div>
-                
-            </section>
-            
-            <Footer2 />
-        </>
-    )
+  return content;
 }
-
